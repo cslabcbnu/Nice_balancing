7f93f08deb8cf3bed319ae70c6d0f0f0179d9cc5
AngryOvO <moonlighthouse99@gmail.com>
2026-01-12 16:33:39 +0900

    ADD:folio_coldcount_step

--- linux-6.15.6/mm/mprotect.c
+++ linux-6.15.6/mm/mprotect.c
@@ -125,9 +125,15 @@ static long change_pte_range(struct mmu_gather *tlb,
 				// hayong
 				if (pte_protnone(oldpte)) {
 					folio = vm_normal_folio(vma, addr, oldpte);
+					printk(KERN_INFO "[KNICE][PROTNONE DETECTED] addr=0x%lx\n",
+					       addr);
 
 					if (folio && folio_test_lru(folio) && folio_nid(folio) == 0) {
+						int before = folio_coldcount(folio);
 						folio_coldcount_inc(folio);
+						int after = folio_coldcount(folio);
+						printk(KERN_INFO "[KNICE][PROTNONE HANDLED] addr=0x%lx nid=%d coldcount: %d -> %d\n",
+						       addr, folio_nid(folio), before, after);
 						}
 						continue;
 					}
