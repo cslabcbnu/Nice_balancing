6251c0d40779233b51775beca354f1e17cb9a254
AngryOvO <moonlighthouse99@gmail.com>
2026-01-22 17:01:15 +0900

    ready

--- linux-6.15.6/kernel/sched/fair.c
+++ linux-6.15.6/kernel/sched/fair.c
@@ -1953,11 +1953,6 @@ bool knice_should_demote(struct task_struct *p, struct folio *folio)
         req_coldcount = 5;
     }
 
-	if(folio_test_cold(folio, req_coldcount))
-	{
-		printk_ratelimited(KERN_INFO "[FOLIO DEMOTE] pid=%d mode = %d coldcount=%d\n", current->pid, mode, folio_coldcount(folio));
-	}
-
     if (!folio_test_cold(folio, req_coldcount))
         return false;
 
