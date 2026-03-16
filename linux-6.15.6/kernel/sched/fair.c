8a5f4ef323c365bb6af9c72b7814b2ee5bfc50b2
AngryOvO <moonlighthouse99@gmail.com>
2026-03-16 14:37:25 +0900

    AutoDemote

--- linux-6.15.6/kernel/sched/fair.c
+++ linux-6.15.6/kernel/sched/fair.c
@@ -1968,6 +1968,12 @@ bool should_numa_migrate_memory(struct task_struct *p, struct folio *folio,
     int dst_nid = cpu_to_node(dst_cpu);
     int last_cpupid, this_cpupid;
 
+	int niceval = task_nice(current);
+
+	if (sysctl_knice_cold_balancing == KNICE_LEVEL_URGENT && niceval >= 0 && src_nid == 1) {
+		return false;
+	}
+
     if (!node_state(dst_nid, N_MEMORY)){
 		return false;
 	}
